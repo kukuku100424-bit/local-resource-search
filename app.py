b5fa34433827a2b9fa4aefbec30878215acd4c35
kukuku100424-bit <kukuku100424@gmail.com>
2026-01-23 02:29:34 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -108,5 +108,8 @@ def search():
 
     return render_template_string(HTML, results=results)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
