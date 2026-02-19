e74539608638f26dc49db9792b968dbce0f35d2b
kukuku100424-bit <kukuku100424@gmail.com>
2026-02-19 15:05:39 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -522,10 +522,11 @@ def desc():
 
     if request.method=="POST":
         query=request.form["query"]
-        print("GPT 추출:", cond)   # ← 이 줄 추가
+    
 
         # GPT 조건 추출
         cond = ai_extract_condition(query)
+        print("GPT 추출:", cond)   # ← 이 줄 추가
 
         cond_display=[]
         if isinstance(cond, dict):
